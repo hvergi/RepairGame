7a6379c480ce3f21867676ca15b4e9c656194893
Hvergi <brewmen@gmail.com>
2020-12-20 20:28:46 -0500

    Added changelog popup when you load a save from an older version.

--- Data/ChangelogData.cs
+++ Data/ChangelogData.cs
@@ -12,11 +12,16 @@ namespace MadnathRepairGame.Data
              new Models.ChangelogArticle("Beta 0.1.1","Dec 18th 2020",
                 "Balance Changes",
                 new Models.ChangeLogItem[] {
+                new Models.ChangeLogItem("Added popup for when a player loads an old save.",Models.ChangeLogItem.ChangeTypes.New),
+
                 new Models.ChangeLogItem("More ql is lost during repairs, making level more important.",Models.ChangeLogItem.ChangeTypes.Change),
                 new Models.ChangeLogItem("Prices of workstations adjusted to match new coin rates.",Models.ChangeLogItem.ChangeTypes.Change),
                 new Models.ChangeLogItem("Affinty chance lowered by a factor of 100.",Models.ChangeLogItem.ChangeTypes.Change),
                 new Models.ChangeLogItem("Rare Coins chance lowered by a factor of 3.",Models.ChangeLogItem.ChangeTypes.Change),
                 new Models.ChangeLogItem("Exp rate nerfed to account for 8 workstations instead of one.",Models.ChangeLogItem.ChangeTypes.Change),
+
+                new Models.ChangeLogItem("Fixed overwriting saveslot 1 if you refreshed the page.",Models.ChangeLogItem.ChangeTypes.Fix),
+
             }),
             new Models.ChangelogArticle("Beta 0.1.0","Dec 18th 2020",
                 "First beta release of MRG",
